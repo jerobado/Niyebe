0657d7b5133175d1c7d0b1a95051cc528f168770
Jero Bado <tokidokitalkyou@gmail.com>
2021-02-18 04:55:00 +0800

    Display simple welcome message

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "NiyebeConfig.h"
+
 
 int main(int argc, char **argv)
 {
 
-    std::cout << "Niyebe\n";
+    std::cout << "Niyebe " << Niyebe_VERSION_MAJOR << "." 
+                           << Niyebe_VERSION_MINOR << "."
+                           << Niyebe_VERSION_PATCH << std::endl;
+    std::cout << "Simple password generator for the forgetful\n";
 
     return 0;
 }
