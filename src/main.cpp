565d2b981b3f9284e2be1ffcfb56b2b058071f91
Jero Bado <tokidokitalkyou@gmail.com>
2021-02-22 04:23:31 +0800

    Created display_welcome_message() function

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "NiyebeConfig.h"
 
 
+
+void display_welcome_message();
 void random_string(int length);
 char random_lowercase(void);
 char random_uppercase(void);
@@ -11,11 +13,18 @@ char random_special(void);
 
 int main(int argc, char **argv)
 {
+    display_welcome_message();
 
+    return 0;
+}
+
+
+
+
+void display_welcome_message()
+{
     std::cout << "Niyebe " << Niyebe_VERSION_MAJOR << "." 
                            << Niyebe_VERSION_MINOR << "."
                            << Niyebe_VERSION_PATCH << std::endl;
     std::cout << "Simple password generator for the forgetful\n";
-
-    return 0;
 }
