febfda601055c448e25e5f756504c838e5a7c65d
Jero Bado <tokidokitalkyou@gmail.com>
2021-02-24 18:22:45 +0800

    Display only welcome message if no arguments entered

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 // for debugging only
 void printd(std::string message);
 
-void display_welcome_message();
+void display_welcome_message(int argc);
 void random_string(int length);
 char random_lowercase(void);
 char random_uppercase(void);
@@ -16,7 +16,7 @@ char random_special(void);
 
 int main(int argc, char **argv)
 {
-    display_welcome_message();
+    display_welcome_message(argc);
 
     int option_char;
     int str_length;
@@ -41,8 +41,6 @@ int main(int argc, char **argv)
                 std::cout << optarg << std::endl;
                 break;
         }
-
-
     }
 
     return 0;
@@ -57,10 +55,13 @@ void printd(std::string message)
 }
 
 
-void display_welcome_message()
+void display_welcome_message(int argc)
 {
-    std::cout << "Niyebe " << Niyebe_VERSION_MAJOR << "." 
-                           << Niyebe_VERSION_MINOR << "."
-                           << Niyebe_VERSION_PATCH << std::endl;
-    std::cout << "Simple password generator for the forgetful\n";
+    if (argc == 1)
+    {
+        std::cout << "Niyebe " << Niyebe_VERSION_MAJOR << "." 
+                               << Niyebe_VERSION_MINOR << "."
+                               << Niyebe_VERSION_PATCH << std::endl;
+        std::cout << "Simple password generator for the forgetful\n";
+    }
 }
