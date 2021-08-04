fe14fb26ff29e26d04be1a10058a9cc3e5609227
Jero Bado <tokidokitalkyou@gmail.com>
2021-08-05 04:05:44 +0800

    Update development version
    Next version will be v1.x.x. We will now be using this in production! #yey

--- include/NiyebeConfig.h
+++ include/NiyebeConfig.h
@@ -1,3 +1,3 @@
-#define Niyebe_VERSION_MAJOR "0"
-#define Niyebe_VERSION_MINOR "1"
-#define Niyebe_VERSION_PATCH "1"
+#define Niyebe_VERSION_MAJOR "1"
+#define Niyebe_VERSION_MINOR "0"
+#define Niyebe_VERSION_PATCH "0a"
