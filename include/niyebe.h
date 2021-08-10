cd23d0f391c3255d906cd9e5d72dab2541bb71f9
Jero Bado <tokidokitalkyou@gmail.com>
2021-08-10 21:07:14 +0800

    Separete implementation by adding niyebe.h header file

---
+++ include/niyebe.h
@@ -0,0 +1,9 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+#include "NiyebeConfig.h"
+
+
+void display_welcome_message(int argc);
+int random_number(int start, int end);
+void random_string(int length);
--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,7 @@
-#include <chrono>
-#include <iostream>
 #include <getopt.h>
-#include <random>
-#include "NiyebeConfig.h"
-#include "ascii_characters.h"
+#include "niyebe.h"
 
 
-// Function declration
-void display_welcome_message(int argc);
-int random_number(int start, int end);
-void random_string(int length);
-
-
-void display_welcome_message(int argc)
-{
-    if (argc == 1)
-    {
-        std::cout << "Niyebe " << Niyebe_VERSION_MAJOR << "." 
-                               << Niyebe_VERSION_MINOR << "."
-                               << Niyebe_VERSION_PATCH << std::endl;
-        std::cout << "Simple pseudo-random password generator for the forgetful\n";
-    }
-}
-
-
-// [] TODO: analyze generated seed if its unique and no duplicate
-int random_number(int start, int end)
-{
-    /* Return a random number between start and end. */
-    
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::mt19937_64 randomizer (seed);
-    std::uniform_int_distribution<int> choices(start, end);
-
-    return choices(randomizer);
-}
-
-
-// [] TODO: add simple string stistics, ie, total length, number of characters, etc.
-void random_string(int length)
-{
-    /* Primary function that will generate a random string based on the given length. */
-
-    for (int i=0; i < length; ++i)
-    {
-        switch(random_number(1, 4))
-        {
-            case 1:
-                std::cout << ASCII_LOWERCASE[random_number(1, 26)];
-                break;
-            case 2:
-                std::cout << ASCII_UPPERCASE[random_number(1, 26)];
-                break;
-            case 3:
-                std::cout << ASCII_DIGIT[random_number(0, 9)];
-                break;
-            case 4:
-                std::cout << ASCII_SYMBOL[random_number(1, 32)];
-                break;
-        }
-    }
-    std::cout << "\n";
-}
-
-
-// ---------------
-// main() is here!
-// ---------------
 int main(int argc, char **argv)
 {
     display_welcome_message(argc);
