609e9c2a84186532eddcc8d5c85e195bb97d3bb9
Jero Bado <programmer@jerobado.com>
2025-04-15 22:43:00 +0800

    Check the result of optional method if has value or not

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char **argv)
 {
     display_welcome_message(argc);
-    parse_command(argc, argv);
- 
+    
+    auto result = parse_command(argc, argv);
+    if (result.has_value())
+        random_string(result.value());
+
     return 0;
 }
